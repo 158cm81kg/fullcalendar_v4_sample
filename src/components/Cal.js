17769c61600beecc3f784d0a1f220d3de61d3825
Tomoya Hayashi <tomoya128@gmail.com>
2019-08-10 00:35:50 +0900

    full cal v4 カスタマイズ

--- src/components/Cal.js
+++ src/components/Cal.js
@@ -1,15 +1,67 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import listPlugin from '@fullcalendar/list'
+import interactionPlugin from '@fullcalendar/interaction' // needed for dayClick
 
-import '../main.scss' // webpack must be configured to do this
+import './main.scss'
 
-export default class Cal extends React.Component {
+export default class DemoApp extends React.Component {
+
+  calendarComponentRef = React.createRef()
+  state = {
+    calendarWeekends: true,
+    calendarEvents: [ // initial event data
+      { title: 'hoge', start: new Date() },
+      { title: 'huga', start: new Date() },
+    ]
+  }
 
   render() {
     return (
-      <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} />
+      <div className='demo-app'>
+        <div className='demo-app-calendar'>
+          <h1>calendar</h1>
+          <FullCalendar
+            defaultView="dayGridMonth"
+            header={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+            }}
+            plugins={[ dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin ]}
+            ref={ this.calendarComponentRef }
+            weekends={ this.state.calendarWeekends }
+            events={ this.state.calendarEvents }
+            dateClick={ this.handleDateClick }
+            />
+        </div>
+      </div>
     )
+  }
+
+  toggleWeekends = () => {
+    this.setState({ // update a property
+      calendarWeekends: !this.state.calendarWeekends
+    })
+  }
+
+  gotoPast = () => {
+    let calendarApi = this.calendarComponentRef.current.getApi()
+    calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
+  }
+
+  handleDateClick = (arg) => {
+    if (window.confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+      this.setState({  // add new event data
+        calendarEvents: this.state.calendarEvents.concat({ // creates a new array
+          title: 'New Event',
+          start: arg.date,
+          allDay: arg.allDay
+        })
+      })
+    }
   }
 
 }
